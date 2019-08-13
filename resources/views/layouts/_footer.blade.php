00929bf86d6391690017e96679376a673752ddb4
gonglin <813822516@qq.com>
2019-08-13 08:38:10 +0000

    路由命名

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
 <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj
 <a href="" target=_blank>刻意练习，每日精进</a>
 <div class="float-right">
-	<a href="/about" >关于</a>
+	<a href="{{ route('about') }}" >关于</a>
 </div>
 </footer>
