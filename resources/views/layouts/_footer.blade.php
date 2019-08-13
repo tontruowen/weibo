4aa0a0c9580fe5cf2b06f4ec67cad67c3cfa39a4
gonglin <813822516@qq.com>
2019-08-13 08:18:30 +0000

    局部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+<img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj
+<a href="" target=_blank>刻意练习，每日精进</a>
+<div class="float-right">
+	<a href="/about" >关于</a>
+</div>
+</footer>
